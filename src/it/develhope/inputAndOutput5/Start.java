c9f19b38a3b3020445de0433238f7bd7b526ffb8
andraxe <99595235+andraxe@users.noreply.github.com>
2022-02-19 00:21:01 +0100

    Initial commit

---
+++ src/it/develhope/inputAndOutput5/Start.java
@@ -0,0 +1,20 @@
+package it.develhope.inputAndOutput5;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+
+Scanner input = new Scanner(System.in);
+
+        System.out.printf("Enter your age: ");
+        int age = input.nextInt();
+        System.out.println(" Your age is : " +age);
+        input.close();
+
+
+
+
+
+    }
+}
